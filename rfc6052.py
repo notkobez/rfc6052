84cd5fe37d10f950990160c67b671db9b51e2148
kobez <k0bez@proton.me>
2023-11-29 23:22:37 -0500

    update ipv4_to_ipv6 error message

--- rfc6052.py
+++ rfc6052.py
@@ -1,14 +1,16 @@
 from ipaddress import ip_address, IPv4Address
 from typing import List
 
+INVALID_IPV4_ERR: str = 'invalid IPv4 address: \'{}\''
+
 def ipv4_to_ipv6(ipv4_address: str) -> str:
     try:
         ip: IPv4Address = ip_address(ipv4_address)
     except ValueError as ve:
-        raise(ve)
+        raise ValueError(INVALID_IPV4_ERR.format(ipv4_address))
     
     if(type(ip) != IPv4Address):
-        raise ValueError('Invalid IPv4 address %s').format(ipv4_address)
+        raise ValueError(INVALID_IPV4_ERR.format(ipv4_address))
          
-    octets: List[int] = list(map(int, ipv4_address.split(".")))
-    return '64:ff9b::{:02x}{:02x}:{:02x}{:02x}'.format(*octets)     
+    octets: List[int] = list(map(int, ipv4_address.split('.')))
+    return '64:ff9b::{:02x}{:02x}:{:02x}{:02x}'.format(*octets)
